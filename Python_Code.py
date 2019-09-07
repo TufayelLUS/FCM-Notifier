3f79f0cfff69c2ca1ea679ae4e6c21c1515de42e
Tufayel Ahmed <39314838+TufayelLUS@users.noreply.github.com>
2019-09-08 00:48:26 +0600

    Create Python_Code.py

---
+++ Python_Code.py
@@ -0,0 +1,28 @@
+# -*- encoding: utf8 -*-
+import requests
+import json
+def sendNotification(user):
+    target = "https://fcm.googleapis.com/fcm/send"
+    header = {
+        "Authorization" : "key=AAAA0JeDn1U:APA91bG2TkugUNq_xg1GlzvpZ5daJvmxpbzupydrWAcM3C6HA9VqtXscgMUShGgWrJE453r8DonmgZmcZ8TPL4VRrLIl8bcAdNTFINN6g7__yf77OPSfS42t7JeTBSNU_aSqj-REbs-k",
+        "Content-Type" : "application/json"
+        }
+    payload = { "notification": {
+        "title": "Ki re mamma ki khobor?",
+        "text": "This text message should be displayed to everyone!",
+         "click_action": ""
+      },
+        "android":{
+      "ttl":"36500s"
+    },
+        "data": {
+        "keyname": "any value "
+        },
+      "to" : user
+    } 
+    x = requests.post(target, data = json.dumps(payload), headers=header)
+    print x.content
+
+users = ['device_token']
+for i in users:
+    sendNotification(i)
